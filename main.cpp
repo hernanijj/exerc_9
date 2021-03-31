01388fc9d804fb7cd031cd679f90a95fe36d163d
hernanijj <42771780+hernanijj@users.noreply.github.com>
2021-03-30 22:27:37 -0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::string entrada;
+
+    std::cout << "Enter with a string" << std::endl;
+    std::getline (std::cin,entrada);
+
+    int cont = 0;
+    for (char ch : entrada)
+    {
+        cont++;
+    }
+
+    std::cout << "The length of the string is: " << cont << std::endl;
+    return 0;
+}
